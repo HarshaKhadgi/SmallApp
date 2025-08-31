5171bdf7e519b2bada13beda851477624e50b890
HarshaKhadgi <harshakhadgi28@gmail.com>
2025-08-31 17:05:18 +0530

    small app created

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.port || 4000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello I am using AWS EKS");
+});
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
